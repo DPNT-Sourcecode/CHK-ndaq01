805de8abbf95b1c63eb3305f45e3e3d49331fca7
sbx_user1051 <sbx_user1051@169.254.189.125>
2019-10-06 14:58:28 +0000

    Sun Oct 06 14:58:28 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,13 +3,29 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    # Assumes string in form '3A 2B'
     special_offers = {
         '3A': 130,
         '2B': 45
     }
+    prices = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
     total = 0
-    # Assumes string in form '3A 2B'
-    for sku in skus.split():
-        if sku in special_offers:
-            total += special_offers[sku]
-        else
+
+    try:
+        for sku in skus.split():
+            if sku in special_offers:
+                total += special_offers[sku]
+            elif sku[1] in prices:
+                total += (prices[sku[1]] * sku[0])
+            else:
+                return -1
+    except Exception:
+        return -1
+    else:
+        return total
+
