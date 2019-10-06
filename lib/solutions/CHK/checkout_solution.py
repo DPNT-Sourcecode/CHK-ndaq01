bc2461a6926f903548a88cf86f2fa914b1e141d1
sbx_user1051 <sbx_user1051@169.254.189.125>
2019-10-06 14:55:36 +0000

    Sun Oct 06 14:55:36 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    special_offers = {
+        '3A': 130,
+        '2B': 45
+    }
+    total = 0
+    # Assumes string in form '3A 2B'
+    for sku in skus.split():
+        if sku in special_offers:
+            total += special_offers[sku]
+        else
