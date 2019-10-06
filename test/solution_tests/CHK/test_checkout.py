c411cbf1b6fe0fba5182d808c2edfe0640947631
sbx_user1051 <sbx_user1051@169.254.25.221>
2019-10-06 17:03:23 +0000

    Sun Oct 06 17:03:23 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -10,13 +10,19 @@ class TestCheckout():
         assert checkout('AAAA') == 180
         assert checkout('AAAAA') == 200
         assert checkout('AAAAAA') == 250
+        assert checkout('AAAAAAAA') == 330
+        assert checkout('AAAAAAAAA') == 380
         assert checkout('AAAAAAAAAA') == 400
 
     def test_checkout_r2_free_B(self):
-        assert checkout ('EE') == 80
+        assert checkout('EE') == 80
         assert checkout('EEB') == 80
         assert checkout('EEBB') == 110
         assert checkout('EEBBB') == 125
+
+    def test_checkout_r2_combined(self):
+        assert checkout('AAAAAEEBAAABB') == 455
+
 
 
 
